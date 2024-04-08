0bb0d1210987b66499de1d0eeab03f0fca800a52
bohac <honza.boh00@gmail.com>
2024-04-08 11:23:45 +0200

    OOP - zaklady, cviceni

---
+++ src/oop/Shopping.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Shopping {
+    public static void main(String[] args) {
+        WineShop winery = new WineShop();
+
+        winery.pricePerBottle = 350;
+        winery.balance = 5000;
+        winery.sell(20);
+
+        winery.buy(15);
+        winery.printBalance();
+        winery.sell(10);
+        winery.printBalance();
+    }
+}
