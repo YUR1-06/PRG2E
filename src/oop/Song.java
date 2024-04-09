37a084f8c09c1df2b92cb5168a340e0b42a60683
bohac <honza.boh00@gmail.com>
2024-04-09 08:41:53 +0200

    OOP - zaklady, konstruktory

--- src/oop/Song.java
+++ src/oop/Song.java
@@ -7,6 +7,14 @@ public class Song {
     double rating;
     int releaseYear;
 
+    public Song(String name, int duration, String composer, double rating, int releaseYear) {
+        this.name = name;
+        this.duration = duration;
+        this.composer = composer;
+        this.rating = rating;
+        this.releaseYear = releaseYear;
+    }
+
     public void printInfo(){
         System.out.println("Song: " + name);
         System.out.println("Written by: " + composer);
--- src/oop/SongsPractice.java
+++ src/oop/SongsPractice.java
@@ -3,18 +3,19 @@ package oop;
 public class SongsPractice {
 
     public static void main(String[] args) {
-        Song paintIt = new Song();
-        paintIt.name = "Paint it, black";
-        paintIt.releaseYear = 1966;
-        paintIt.composer = "The Rolling stones";
-        paintIt.rating = 9.3;
-        paintIt.duration = 227;
+        Song paintIt = new Song("Paint it, black",
+                227, "The Rolling stones", 9.5, 1966);
+//        paintIt.name = "Paint it, black";
+//        paintIt.releaseYear = 1966;
+//        paintIt.composer = "The Rolling stones";
+//        paintIt.rating = 9.3;
+//        paintIt.duration = 227;
         paintIt.printInfo();
 
-        Song dream = new Song();
-        dream.name = "Dream on";
-        dream.composer = "Aerosmith";
-        dream.printInfo();
+//        Song dream = new Song();
+//        dream.name = "Dream on";
+//        dream.composer = "Aerosmith";
+//        dream.printInfo();
     }
 
 }
